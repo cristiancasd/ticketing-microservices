9539be5ff6893ef3524dafe0b6691be4544753b4
Cristian Salazar <cristiancasd@gmail.com>
2023-02-27 12:39:23 -0500

    news start up messages to test

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,13 +2,13 @@ import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   return currentUser 
-  ? (<h1>You are signed in</h1>) 
-  : (<h1>You are NOT signed in</h1>);
+  ? (<h1>You are signed in Digital Ocean</h1>) 
+  : (<h1>You are NOT signed in  Digital Ocean</h1>);
 }; 
 
 
 LandingPage.getInitialProps = async (context) => {
-  console.log('LANDING PAGE!');
+  console.log('LANDING PAGE  Digital Ocean!');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
   return data;
